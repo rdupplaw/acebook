c4c6b4cbb700ea8ac39567911f26d3a54eb30c18
Maria Valero <mvalerogonzalez@gmail.com>
2021-06-15 20:36:48 +0100

    new user controller and erb.html added

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
+  get 'user/new'
   get "/articles", to: "articles#index"
+  get "/signup", to: "user#new"
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
---
+++ spec/controllers/user_controller_spec.rb
@@ -0,0 +1,10 @@
+require 'rails_helper'
+
+RSpec.describe User, type: :controller do
+  pending "add some examples to (or delete) #{__FILE__}"
+  
+  it "should get new" do
+    get signup_path
+    assert_response :successs
+  end
+end
