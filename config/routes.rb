fe4e5f82ad031cfeda24585eaf7cb16c4fc8a12b
Felix Piercy <74382928+fel1xp@users.noreply.github.com>
2021-06-16 12:36:41 +0100

    WIP - User Signup incorrect routing

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
 Rails.application.routes.draw do
-  get 'user/new'
-  get "/sign_up", to: "user#new"
+#  get 'user/new'
+ # get "/sign_up", to: "user#new"
+  
+ resources :posts, :users
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
--- spec/features/signup_page_spec.rb
+++ spec/features/signup_page_spec.rb
@@ -2,7 +2,13 @@ require 'rails_helper'
 
 feature 'Visitors can sign up' do
   scenario 'via email' do
-    visit sign_up_path
+    visit '/users/new'
     expect(page).to have_content("Email")
+    fill_in "user_firstname", with: "John"
+    fill_in "user_lastname", with: "Doe"
+    fill_in "user_email", with: "john@test.com"
+    fill_in "user_password", with: "test123"
+    click_button "Create account"
+    expect(User.count).to eq(1)
   end
 end
