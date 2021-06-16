83cb8d0a23f1196919c8054602e138691c5288fd
Felix Piercy <74382928+fel1xp@users.noreply.github.com>
2021-06-16 14:40:04 +0100

    User Sign-Up Complete

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,11 @@
 class UsersController < ApplicationController
   def new 
+    @user = User.new 
+  end
+
+  def create
+    @user = User.create(user_params)
+    redirect_to posts_url
   end
   
   private
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,2 @@
 class User < ApplicationRecord
-  attr_accessor :email, :password, :firstname, :lastname
-
-  def initialize(attributes = {})
-    @email = attributes[:email]
-    @password = attributes[:password]
-    @firstname = attributes[:firstname]
-    @lastname = attributes[:lastname]
-  end
-
 end
