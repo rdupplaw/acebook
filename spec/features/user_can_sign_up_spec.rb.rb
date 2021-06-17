f64a27e05a32ceb8da412480ed73c3bf8f9a2895
Robert Dupplaw <dupplawrobert@gmail.com>
2021-06-17 12:17:02 +0100

    Validates firstname present

--- spec/features/user_can_sign_up_spec.rb.rb
+++ spec/features/user_can_sign_up_spec.rb.rb
@@ -8,6 +8,7 @@ feature 'Sign up'' do
     fill_in "user_lastname", with: "Doe"
     fill_in "user_email", with: "john@test.com"
     fill_in "user_password", with: "test123"
+    fill_in "user_password_confirmation", with: "test123"
     click_button "Create account"
     expect(User.count).to eq(1)
   end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,12 +1,20 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  before(:each) do
+    @user = User.new(firstname: 'Example', lastname: 'User', email: 'user@example.com')
+  end
 
-  describe "#create" do
-    it "creates a user" do
-      user = User.new(email:"me@mail.com", password:'1234' , firstname:'Jane' , lastname:'Doe')
-      expect(user.email).to eq("me@mail.com")
+  context 'when given all required attributes' do
+    it 'is valid' do
+      expect(@user).to be_valid
+    end
+  end
+
+  context 'when not given a firstname' do
+    it 'is invalid' do
+      @user.firstname = ''
+      expect(@user).not_to be_valid
     end
   end
 end
