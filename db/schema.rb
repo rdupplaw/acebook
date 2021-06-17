fa8990d2eb9074f5f3f805a859b7c6236060e4cb
Robert Dupplaw <dupplawrobert@gmail.com>
2021-06-17 15:18:00 +0100

    Replaced password field with password_digest

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_06_17_135250) do
+ActiveRecord::Schema.define(version: 2021_06_17_141648) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -35,9 +35,9 @@ ActiveRecord::Schema.define(version: 2021_06_17_135250) do
     t.string "email"
     t.string "firstname"
     t.string "lastname"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
