e46b7e75c5833e6d48414758fb3fc5388667ed05
Robert Dupplaw <36577903+rdupplaw@users.noreply.github.com>
2021-06-17 10:20:08 +0100

    Fix: added end to block

--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 2021_06_15_135354) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["post_id"], name: "index_comments_on_post_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "message"
