eadcaf9a105bbd7d3f152667696fdbb0dee51377
Robert Dupplaw <dupplawrobert@gmail.com>
2021-06-18 15:21:17 +0100

    added password_digest to schema

--- db/schema.rb
+++ db/schema.rb
@@ -63,9 +63,10 @@ ActiveRecord::Schema.define(version: 2021_06_18_104257) do
     t.string "email"
     t.string "firstname"
     t.string "lastname"
-    t.string "password"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "password_digest"
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
   add_foreign_key "active_storage_attachments", "active_storage_blobs", column: "blob_id"
