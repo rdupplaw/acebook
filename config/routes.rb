8428b40c8a798cde7d6cb9c9b25f93d6d9f5584a
Maria Valero <mvalerogonzalez@gmail.com>
2021-06-18 16:35:15 +0100

    Basic styling for main page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,14 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
- 
- resources :users
- resources :stylesheets
-  
- resources :posts do
-    resources :comments
-    get '/like' , on: :member, to: 'posts#like'
- end
+   get '/login', to: 'sessions#new'
+   post '/login', to: 'sessions#create'
+   delete '/logout', to: 'sessions#destroy'
 
+   resources :users
+  
+   resources :posts do
+      resources :comments
+      get '/like' , on: :member, to: 'posts#like'
+   end
 end
