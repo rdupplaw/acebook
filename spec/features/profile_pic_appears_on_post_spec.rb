8f5d36ac7e50eff03fc76d915a1f7c5a6f2aafc2
Willoughby Hood <willoughbyhood29@googlemail.com>
2021-06-28 16:19:47 +0100

    profile_pic appear next to post along with name

---
+++ spec/features/profile_pic_appears_on_post_spec.rb
@@ -0,0 +1,15 @@
+require 'rails_helper'
+
+RSpec.describe 'Timeline', type: :feature do
+  before do
+    register_and_login
+  end
+
+  it 'Can submit posts and view them' do
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, world!'
+    click_button 'Submit'
+    expect(page).to have_css("img[src*='profile_pic.jpeg']")
+  end
+
+end
