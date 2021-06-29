ddbe00abbcc206c2282a403dc865f3d45f83cb0b
Charlie Slater <charliemslater1701@gmail.com>
2021-06-29 15:01:52 +0100

    tests after registering second user

--- spec/features/profile_pic_appears_on_post_spec.rb
+++ spec/features/profile_pic_appears_on_post_spec.rb
@@ -13,4 +13,15 @@ RSpec.describe 'Timeline', type: :feature do
     click_button 'Submit'
     expect(page).to have_css("img[src*='profile_pic.jpeg']")
   end
+
+  it 'Displays other users profile pic next to post' do
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, world!'
+    click_button 'Submit'
+    click_link 'Log Out'
+
+    register_second_user
+
+    expect(page).to have_css("img[src*='profile_pic.jpeg']")
+  end
 end
--- spec/features/register_and_login.rb
+++ spec/features/register_and_login.rb
@@ -21,3 +21,14 @@ def register
   fill_in 'user_password_confirmation', with: 'test123'
   click_button 'Create account'
 end
+
+def register_second_user
+  visit '/users/new'
+  attach_file 'user_profile_picture', 'spec/files/3000.jpeg'
+  fill_in 'user_firstname', with: 'James'
+  fill_in 'user_lastname', with: 'Doe'
+  fill_in 'user_email', with: 'james@example.com'
+  fill_in 'user_password', with: 'test123'
+  fill_in 'user_password_confirmation', with: 'test123'
+  click_button 'Create account'
+end
