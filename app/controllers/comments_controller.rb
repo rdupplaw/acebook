1e585935d55ce1c9abeb2046b6a1b033c9452661
charlie-casserly <charliecasserly215@gmail.com>
2021-06-29 09:05:11 +0100

    small change to controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,7 +1,7 @@
 class CommentsController < ApplicationController
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(comment_params.merge(user_id: current_user.id))
+    @comment = @post.comments.create(comment_params)
     redirect_to post_url(@post)
   end
 
@@ -14,6 +14,6 @@ class CommentsController < ApplicationController
 
   private
   def comment_params
-    params.require(:comment).permit(:body)
+    params.require(:comment).permit(:body).merge(user_id: current_user.id)
   end
 end
