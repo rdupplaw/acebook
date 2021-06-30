ba3fa43faa924cb195c5169a015c71e2d8cdc21a
Willoughby Hood <willoughbyhood29@googlemail.com>
2021-06-30 15:00:05 +0100

    more testing

--- spec/features/users/users_can_see_their_posts_on_their_profile_spec.rb
+++ spec/features/users/users_can_see_their_posts_on_their_profile_spec.rb
@@ -8,8 +8,17 @@ RSpec.describe 'Profile page', type: :feature do
     click_link 'New post'
     fill_in 'Message', with: 'Hello, world!'
     click_button 'Submit'
-    click_link "Profile"
+    click_link 'My Profile'
     expect(page).to have_content("Hello, world!")
+  end
 
+  it 'does not show other peoples posts' do
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, world!'
+    click_button 'Submit'
+    click_link "Log Out"
+    register_second_user
+    click_link "My Profile"
+    expect(page).not_to have_content("Hello, world!")
   end
 end
