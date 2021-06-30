44a7e2dd5bb5eed3e7da38995a7c5f4fc75b1ccc
charlie-casserly <charliecasserly215@gmail.com>
2021-06-30 11:26:38 +0100

    feature test written

--- spec/features/comments/comments_can_be_deleted_spec.rb
+++ spec/features/comments/comments_can_be_deleted_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe 'Comments', type: :feature do
     register
   end
 
-  it 'Can be deleted' do
+  it 'Can delete own comments' do
     click_link 'New post'
     fill_in 'Message', with: 'Hello, world!'
     click_button 'Submit'
@@ -23,5 +23,22 @@ RSpec.describe 'Comments', type: :feature do
     expect(page).not_to have_content("TestUser")
     expect(page).not_to have_content("goodbye")
 
+  end
+
+  it 'Cannot delete other users comments' do
+    click_link 'New post'
+    fill_in 'Message', with: 'Hello, world!'
+    click_button 'Submit'
+    click_link 'View'
+
+    fill_in 'comment[body]', with: 'goodbye'
+    click_button 'Create Comment'
+
+    click_link 'Log Out'
+
+    register_second_user
+
+    click_link "View"
+    expect(page).not_to have_link 'Delete'
   end
 end
